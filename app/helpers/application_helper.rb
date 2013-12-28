4a0ad1b0182c9d9d81024e3fe1ccd326758157bf
Jan Kechel <jan@kechel.de>
2013-12-28 03:40:23 +0000

    bugfix missing 1st line

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+module ApplicationHelper
+
   def newline2br(str)
     if !str
       return ''

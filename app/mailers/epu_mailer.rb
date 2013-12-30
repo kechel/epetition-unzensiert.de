58295a8caa424d00df0d79c290cea0e272a36a14
Jan Kechel <jan@kechel.de>
2013-12-30 15:56:57 +0000

    bugfix missing 1st line

--- app/mailers/epu_mailer.rb
+++ app/mailers/epu_mailer.rb
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+class EpuMailer < Devise::Mailer  
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   
